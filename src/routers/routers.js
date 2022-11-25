ee284bf978fd3a75f9e8fde3ed125aa7db7ccec7
Tobi Kesuma <tobikesuma1999@gmail.com>
2022-11-26 05:51:54 +0800

    new begins

---
+++ src/routers/routers.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    Routes,
+    Route,
+} from 'react-router-dom'
+
+
+
+
+const routers = () => {
+  return (
+    <Routes>
+        
+    </Routes>
+  )
+}
+
+export default routers
